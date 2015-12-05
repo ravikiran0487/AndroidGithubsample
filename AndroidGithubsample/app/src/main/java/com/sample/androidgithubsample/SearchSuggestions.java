f8b6c9b35d5db0818d5ba1f44663c9acf5c672f4
ravi <ravi.kiran@krvsoft.com>
2015-12-05 17:00:42 +0530

    fifth commit

---
+++ AndroidGithubsample/app/src/main/java/com/sample/androidgithubsample/SearchSuggestions.java
@@ -0,0 +1,15 @@
+package com.sample.androidgithubsample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by KRV6User on 12/5/2015.
+ */
+public class SearchSuggestions extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.searchsuggestions);
+    }
+}
--- AndroidGithubsample/app/src/main/java/com/sample/androidgithubsample/TextToSpeechExample.java
+++ AndroidGithubsample/app/src/main/java/com/sample/androidgithubsample/TextToSpeechExample.java
@@ -1,19 +1,71 @@
 package com.sample.androidgithubsample;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 
 /**
  * Created by KRV6User on 12/5/2015.
  */
-public class TextToSpeechExample extends AppCompatActivity{
+public class TextToSpeechExample extends AppCompatActivity {
 
     Button btnspeak;
+    Handler handler;
+    TextToSpeech tts=null;
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.texttospeechexample);
+        btnspeak = (Button) this.findViewById(R.id.btnspeak);
+        handler = new Handler();
+        pd = new ProgressDialog(this);
+        pd.setMessage("loading");
+        pd.show();
+        new myThread().start();
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (tts!=null&&tts.isSpeaking()) {
+            tts.stop();
+            tts.shutdown();
+        }
+    }
+
+    public class myThread extends Thread implements Runnable {
+        @Override
+        public void run() {
+            super.run();
+            tts = new TextToSpeech(TextToSpeechExample.this, new TextToSpeech.OnInitListener() {
+                @Override
+                public void onInit(int status) {
+                    if (status == TextToSpeech.SUCCESS) {
+                        tts.setPitch(1.0f);
+                        try {
+                            Thread.sleep(4000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (pd.isShowing()) {
+                                    pd.dismiss();
+                                }
+                                tts.speak("Welcome to android", TextToSpeech.QUEUE_ADD, null);
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    }
+
+
 }
