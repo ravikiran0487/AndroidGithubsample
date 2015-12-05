dd815baa2ef69ed66e0d0c81f0f879e1d46706c8
ravi <ravi.kiran@krvsoft.com>
2015-12-05 14:31:06 +0530

    fifth commit

---
+++ AndroidGithubsample/app/src/main/java/com/sample/androidgithubsample/ContentResolverExample.java
@@ -0,0 +1,18 @@
+package com.sample.androidgithubsample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by KRV6User on 12/2/2015.
+ */
+public class ContentResolverExample extends AppCompatActivity{
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contentresolverexample);
+    }
+}
---
+++ AndroidGithubsample/app/src/main/java/com/sample/androidgithubsample/TextToSpeechExample.java
@@ -0,0 +1,19 @@
+package com.sample.androidgithubsample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+/**
+ * Created by KRV6User on 12/5/2015.
+ */
+public class TextToSpeechExample extends AppCompatActivity{
+
+    Button btnspeak;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.texttospeechexample);
+    }
+}
