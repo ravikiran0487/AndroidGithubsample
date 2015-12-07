d36a86d2df144df77205215f5a29d4449ae79de6
ravi <ravi.kiran@krvsoft.com>
2015-12-07 18:37:21 +0530

    fifth commit

--- AndroidGithubsample/app/src/main/java/com/sample/androidgithubsample/SearchSuggestions.java
+++ AndroidGithubsample/app/src/main/java/com/sample/androidgithubsample/SearchSuggestions.java
@@ -1,15 +1,39 @@
 package com.sample.androidgithubsample;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 /**
  * Created by KRV6User on 12/5/2015.
  */
-public class SearchSuggestions extends AppCompatActivity{
+public class SearchSuggestions extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.searchsuggestions);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.search,menu);
+        SearchManager searchManager= (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView=(SearchView)menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.search){
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
